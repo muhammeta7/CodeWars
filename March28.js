aec3b6e3c9b93ae3449863ec3fce72f9521d90e7
Muhammet <muhammeta7@gmail.com>
2017-03-28 18:56:39 -0400

    get middle character exercise completed

--- March28.js
+++ March28.js
@@ -8,3 +8,10 @@ function dontGiveMeFive(start, end){
   }
   return result.length;
 }
+
+// Get the Middle Character
+// You are going to be given a word. Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+function getMiddle(s) {
+  return s.substring(Math.ceil(s.length/2)-1, Math.floor(s.length/2) + 1);
+}
