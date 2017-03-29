6071ddb1fbce9beae573be59d58c12f5d6fdd1ab
Muhammet <muhammeta7@gmail.com>
2017-03-28 21:32:35 -0400

    findNextSqrt function

--- March28.js
+++ March28.js
@@ -15,3 +15,22 @@ function dontGiveMeFive(start, end){
 function getMiddle(s) {
   return s.substring(Math.ceil(s.length/2)-1, Math.floor(s.length/2) + 1);
 }
+
+// Find the next perfect square
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. 
+// Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+function findNextSquare(sq) {
+  // Return the next square if sq if a perfect square, -1 otherwise
+  if (Math.sqrt(sq) % 1 = 0){
+    var sqRoot = Math.sqrt(sq) + 1;
+    return Math.pow(sqRoot,2)
+  }
+  else {
+    return -1;
+  }
+}
+// Best practice solution
+function findNextSquare(sq) {
+  return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+}
