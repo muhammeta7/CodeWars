9999450fb9c4a18d4a3ac8fafbdd56a35052fc00
Muhammet <muhammeta7@gmail.com>
2017-03-29 02:07:41 -0400

    create phoneNumber out of array

--- March28.js
+++ March28.js
@@ -7,14 +7,14 @@ function dontGiveMeFive(start, end){
     if (!i.toString().includes('5')) result.push(i);
   }
   return result.length;
-}
+};
 
 // Get the Middle Character
 // You are going to be given a word. Your job is to return the middle character of the word. 
 // If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 function getMiddle(s) {
   return s.substring(Math.ceil(s.length/2)-1, Math.floor(s.length/2) + 1);
-}
+};
 
 // Find the next perfect square
 // Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. 
@@ -29,8 +29,28 @@ function findNextSquare(sq) {
   else {
     return -1;
   }
-}
+};
+
 // Best practice solution
 function findNextSquare(sq) {
   return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+};
+
+
+// Create Phone Number
+// Write a function that accepts an array of 10 integers (between 0 and 9), 
+// that returns a string of those numbers in the form of a phone number.
+function createPhoneNumber(numbers){
+  return "(" + numbers.slice(0,3).join("") + ") " + numbers.slice(3,6).join("") + "-" + numbers.slice(6,10).join("");
 }
+// refactored
+function createPhoneNumber(numbers){
+  numbers = numbers.join('');
+  return '(' + numbers.substring(0, 3) + ') ' 
+      + numbers.substring(3, 6) 
+      + '-' 
+      + numbers.substring(6);
+}
+
+// Test Code
+Test.assertEquals(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
