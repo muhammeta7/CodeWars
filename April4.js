7906da953908750298c1f556fb3ec9870184e72d
Muhammet <muhammeta7@gmail.com>
2017-04-04 21:27:18 -0400

    supermarket que complete, also trouble understanding

--- April4.js
+++ April4.js
@@ -12,3 +12,27 @@ function goodVsEvil(good, evil){
 Test.expect( goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1') === 'Battle Result: Evil eradicates all trace of Good', 'Evil should win' );
 Test.expect( goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0') === 'Battle Result: Good triumphs over Evil', 'Good should win' );
 Test.expect( goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0') === 'Battle Result: No victor on this battle field', 'Should be a tie' );
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+// The SuperMarket Que
+function queueTime(customers, n) {
+  var ticks = 0;
+  while(customers.length) {
+    ticks++;
+  
+    for(var i = Math.min(customers.length, n) - 1; i >= 0; i--) {
+      customers[i]--;
+      if(!customers[i]) {
+        customers.splice(i, 1)
+      }
+    }
+  } 
+  return ticks;
+}
+
+// Testing Code
+Test.describe("example tests", function() {
+Test.assertEquals(queueTime([], 1), 0);
+Test.assertEquals(queueTime([1,2,3,4], 1), 10);
+Test.assertEquals(queueTime([2,2,3,3,4,4], 2), 9);
+Test.assertEquals(queueTime([1,2,3,4,5], 100), 5);
